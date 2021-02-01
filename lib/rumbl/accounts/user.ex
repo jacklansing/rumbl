261d3e5467362bd01ad08e6c3a94da4a3cd04b20
Jack Lansing <jack.lansing@live.com>
2021-02-01 02:47:24 -0500

    ch7 -- add categories to videos along with *_constraint changeset functions + seed data for categories

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -16,6 +16,7 @@ defmodule Rumbl.Accounts.User do
     |> cast(attr, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
   end
 
   def registration_changeset(user, params) do
