55736db860d00b99b5c56b62e2bcdb1b9e18d482
Jack Lansing <jack.lansing@live.com>
2021-02-08 16:56:57 -0500

    finish up test examples

--- config/test.exs
+++ config/test.exs
@@ -20,3 +20,6 @@ config :rumbl, RumblWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Reduce password hashing rounds during testing to speed up tests
+config :pbkdf2_elixir, :rounds, 1
