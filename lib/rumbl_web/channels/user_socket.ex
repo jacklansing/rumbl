c3b642c50a2d16045c249e7fce955b66a2dfee8d
Jack Lansing <jack.lansing@live.com>
2021-02-16 22:24:18 -0500

    token auth added for ws, basic channels setup for videos, add schema for video annotations

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -1,8 +1,25 @@
 defmodule RumblWeb.UserSocket do
   use Phoenix.Socket
 
+  @max_age 2 * 7 * 24 * 60 * 60
+
   ## Channels
-  # channel "room:*", RumblWeb.RoomChannel
+  channel "videos:*", RumblWeb.VideoChannel
+
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+           socket,
+           "user socket",
+           token,
+           max_age: @max_age
+         ) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
+  end
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
